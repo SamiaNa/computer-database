47905fcf7dbd9816eb8f6b9823dc6206ec4a897c
Samia Nabili <snabili@excilys.com>
2018-06-05 11:18:44 +0200

    [FIX] url service webapp

--- webapp/src/main/java/com/excilys/formation/controller/ComputerDatabaseController.java
+++ webapp/src/main/java/com/excilys/formation/controller/ComputerDatabaseController.java
@@ -18,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.excilys.formation.binding.mappers.CompanyDTOMapper;
